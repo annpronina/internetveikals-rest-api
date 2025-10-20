edd17b2ec5e8fbbce64dab3d4f6c4847c3b3783f
Ricards Almanis <ricardsalmanis@gmail.com>
2025-10-20 16:53:21 +0300

    model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'brand_id', 'discount_id'];
+
+    public function category() { return $this->belongsTo(Category::class); }
+    public function brand() { return $this->belongsTo(Brand::class); }
+    public function discount() { return $this->belongsTo(Discount::class); }
+    public function tags() { return $this->belongsToMany(Tag::class); }
 }
